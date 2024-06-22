2614754bd68af3542c295771cf307c03441aefcf
Koki-T3 <koki@air360.io>
2024-06-22 09:15:50 +0900

    Project setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import CalendarSection from "./components/calendar-section";
+
+function App() {
+  return (
+    <>
+      <div className="">
+        <CalendarSection />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/calendar-section.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Calendar } from "./ui/calendar";
+
+const CalendarSection = () => {
+  const [date, setDate] = React.useState<Date | undefined>(new Date());
+  return (
+    <Calendar
+      mode="single"
+      selected={date}
+      onSelect={setDate}
+      className="rounded-md border"
+    />
+  );
+};
+
+export default CalendarSection;
